28cc4300654a1b59383622363a5f36a99346d78d
Matthew Walcher <mwalcher@live.ca>
2017-07-08 12:51:12 -0400

    added typography styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
     <meta name="twitter:description" content="{!! __('meta.description') !!}">
     <meta name="twitter:image" content="{{ asset('images/') }}{{ __('meta.image.src') }}">
 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,700" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
