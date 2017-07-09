bf7df4bfaf03fe5d1680a3b82f39fbf2af5148da
Matthew Walcher <mwalcher@live.ca>
2017-07-08 20:23:22 -0400

    added link and button styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,12 +36,12 @@
     <meta name="twitter:description" content="{!! __('meta.description') !!}">
     <meta name="twitter:image" content="{{ asset('images/') }}{{ __('meta.image.src') }}">
 
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,400" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <h1>{!! __('meta.title') !!}</h1>
-
+    
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
