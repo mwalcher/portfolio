e077d3ad53abf3ddb459bed18085d17c06179dd3
Matthew Walcher <mwalcher@live.ca>
2017-07-10 23:11:12 -0400

    added header base styles

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <a href="/" class="logo no-hover" title="Home">
+        <img src="images/mwalcher-logo-light.svg" alt="mwalcher logo" />
+    </a>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    @component('components/header')@endcomponent
 
     @yield('hero')
 
