0f2d21048fa45e0c5f31e064099c8add1894a259
Matthew Walcher <mwalcher@live.ca>
2017-07-17 21:53:05 -0400

    added tab content container component and toggle active javascript component

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,4 +4,25 @@
         <h2>{!! __($page.'.hero.sub-title') !!}</h2>
         <p>{!! __($page.'.hero.content') !!}</p>
     </div>
+
+    <div class="tab-content">
+        <div class="tab-container">
+            <button class="tab active" data-toggle="skills" title="Skills">Skills</button>
+            <button class="tab" data-toggle="tools" title="Tools">Tools</button>
+        </div>
+
+        <div class="tab-content-container">
+            <div class="content-container active" data-toggle-content="skills">
+                <p>
+                    Skills
+                </p>
+            </div>
+
+            <div class="content-container" data-toggle-content="tools">
+                <p>
+                    Tools
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
