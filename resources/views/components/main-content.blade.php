75d2fe1e6b13582159898702303cdfe0c967b52b
Matthew Walcher <mwalcher@live.ca>
2017-08-14 22:50:24 -0400

    added base structure and base styles for featured project section

--- resources/views/components/main-content.blade.php
+++ resources/views/components/main-content.blade.php
@@ -2,10 +2,25 @@
     <h2>{!! __($page.'.main-content.main-title') !!}</h2>
 
     @if(__($page.'.main-content.featured-content'))
+    <?php
+    $projects = __($page.'.main-content.featured-content');
+    $default = reset($projects);
+    ?>
     <div class="featured-content">
-        @foreach (__($page.'.main-content.featured-content') as $project)
-            {!! $project['name'] !!}
-        @endforeach
+        <div class="absolute-background" style="background-image: url({{ $default['image'] }})"></div>
+        <div class="content">
+            <p class="title">{!! $default['name'] !!}</p>
+            <p class="description">{!! $default['description'] !!}</p>
+            <a href="{!! $default['link'] !!}" class="button" title="View {!! $default['name'] !!}">View Project</a>
+        </div>
     </div>
+
+    <ul class="project-list">
+        @foreach (__($page.'.main-content.featured-content') as $project)
+        <li>
+            {!! $project['name'] !!}
+        </li>
+        @endforeach
+    </ul>
     @endif
 </section>
