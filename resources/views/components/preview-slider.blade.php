5f829890db073fc2c0d1fcf25d7384cb54bfac43
Matthew Walcher <mwalcher@live.ca>
2017-12-03 21:58:43 -0500

    added base styles for preview slider

--- resources/views/components/preview-slider.blade.php
+++ resources/views/components/preview-slider.blade.php
@@ -1,7 +1,7 @@
 <div class="preview-slider-container">
     <ul class="preview-slider">
         @foreach ($properties['list'] as $key => $listItem)
-        <li>
+        <li class="slide">
             <img src="{{$listItem['src']}}" alt="{{$listItem['alt']}}" />
         </li>
         @endforeach
