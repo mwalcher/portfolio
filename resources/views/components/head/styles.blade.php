76d12b0ea983a6848d681cf109915f218078b572
Matthew Walcher <mwalcher@live.ca>
2019-01-08 23:28:52 -0500

    increased font weight of body bold text

--- resources/views/components/head/styles.blade.php
+++ resources/views/components/head/styles.blade.php
@@ -1,4 +1,4 @@
-<link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,400" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,500" rel="stylesheet">
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 <noscript>
