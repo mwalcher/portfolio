cc16a98adb3c86b8bd354ed64b2132bfa806fde0
Matthew Walcher <mwalcher@live.ca>
2019-01-12 21:24:36 -0500

    moved about block back

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -36,14 +36,6 @@ return [
         'content' => 'I am a detail oriented problem solver who is determined to find a solution even if it does not come easily.'
     ],
     'tab-content' => [
-        'about' => [
-            'title' => 'About Me',
-            'content' => [
-                'Welcome! I appreciate your interest, so allow me to introduce myself. I am Matthew Walcher and I am currently working as an intermediate front-end web developer at <a class="animated-hover" href="https://thearcane.com/" target="_blank" rel="noopener" title="Arcane website">Arcane</a>.',
-                'During my time at Arcane I have earned my reputation for quality because of my strong work ethic. When I am given a task, I start by thinking through the problem first before I start to build to ensure I have the right solution the first time. I love to collaborate and bounce ideas off my peers when working through a problem because I know I can benefit from other perspectives on how to solve the problem.',
-                'I stumbled into my passion for front-end web development through my love for art and design. Art was always my favourite subject when I was growing up, even though I eventually realized I wasn\'t a naturally gifted artist. In my early to mid teens I began to dabble in photography and photoshop, which lead to an interest in graphic design. I taught myself basic HTML and CSS because I wanted to design my own custom layouts on Neopets, a popular virtual pet site. After high school, I decided to study graphic design and web development as part of a collaborative diploma and degree program between Western University and Fanshawe College. When I was going into the program I wanted to be a graphic designer, but quickly realized my natural talent and passion was in web development. I excelled in our coding courses and was often helping my peers with the assignments (and they would help me with my designs).'
-            ]
-        ],
         'tools' => [
             'title' => 'What I Use',
             'list' => [
@@ -63,6 +55,14 @@ return [
                     'label' => 'Vue.js',
                     'link' => 'https://vuejs.org/'
                 ]
+            ]
+        ],
+        'about' => [
+            'title' => 'About Me',
+            'content' => [
+                'Welcome! I appreciate your interest, so allow me to introduce myself. I am Matthew Walcher and I am currently working as an intermediate front-end web developer at <a class="animated-hover" href="https://thearcane.com/" target="_blank" rel="noopener" title="Arcane website">Arcane</a>.',
+                'During my time at Arcane I have earned my reputation for quality because of my strong work ethic. When I am given a task, I start by thinking through the problem first before I start to build to ensure I have the right solution the first time. I love to collaborate and bounce ideas off my peers when working through a problem because I know I can benefit from other perspectives on how to solve the problem.',
+                'I stumbled into my passion for front-end web development through my love for art and design. Art was always my favourite subject when I was growing up, even though I eventually realized I wasn\'t a naturally gifted artist. In my early to mid teens I began to dabble in photography and photoshop, which lead to an interest in graphic design. I taught myself basic HTML and CSS because I wanted to design my own custom layouts on Neopets, a popular virtual pet site. After high school, I decided to study graphic design and web development as part of a collaborative diploma and degree program between Western University and Fanshawe College. When I was going into the program I wanted to be a graphic designer, but quickly realized my natural talent and passion was in web development. I excelled in our coding courses and was often helping my peers with the assignments (and they would help me with my designs).'
             ]
         ]
     ],
