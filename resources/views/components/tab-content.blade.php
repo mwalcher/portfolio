5552d2677e3d821398ca9822fa17c043179704c5
Matthew Walcher <mwalcher@live.ca>
2019-03-01 23:16:16 -0500

    removed scroll bar from icon list content

--- resources/views/components/tab-content.blade.php
+++ resources/views/components/tab-content.blade.php
@@ -1,15 +1,41 @@
 <div class="tab-content">
     <div class="tab-container">
         @foreach ($list as $listItem)
-        <button class="tab @if($loop->index == 0)active @endif" data-toggle="{!! $listItem['title'] !!}" data-parent="tab-content" title="{!! $listItem['title'] !!}">{!! $listItem['title'] !!}</button>
+        @php
+            $buttonClasses = ['tab'];
+            if($loop->index == 0){
+                array_push($buttonClasses, 'active');
+            }
+        @endphp
+        <button
+            class="{{ implode(' ', $buttonClasses) }}"
+            data-toggle="{!! $listItem['title'] !!}"
+            data-parent="tab-content"
+            title="{!! $listItem['title'] !!}"
+        >{!! $listItem['title'] !!}</button>
         @endforeach
     </div>
 
     <div class="tab-content-container">
         @foreach ($list as $listItem)
-        <div class="content-container @if($loop->index == 0)active @endif" data-toggle-content="{!! $listItem['title'] !!}"  data-parent="tab-content">
-            <div class="content">
+        @php
+            $containerClasses = ['content-container'];
+            $contentClasses = ['content'];
+            if($loop->index == 0){
+                array_push($containerClasses, 'active');
+            }
+            if(isset($listItem['list'])){
+                array_push($contentClasses, 'no-scroll');
+            }
+        @endphp
+        <div
+            class="{{ implode(' ', $containerClasses) }}"
+            data-toggle-content="{!! $listItem['title'] !!}"
+            data-parent="tab-content"
+        >
+            <div class="{{ implode(' ', $contentClasses) }}">
                 <h2 class="invisible">{!! $listItem['title'] !!}</h2>
+
                 @if (isset($listItem['content']))
                     @foreach ($listItem['content'] as $key => $content)
                     <p>{!! $content !!}</p>
@@ -22,14 +48,23 @@
                     <div class="icon-container">
                         <div class="shape">
                             <div class="icon">
-                                <div class="image" style="background-image:url('{!! asset('images') !!}/icon-{!! $key !!}.svg');"></div>
+                                <div
+                                    class="image"
+                                    style="background-image:url('{!! asset('images') !!}/icon-{!! $key !!}.svg');"
+                                ></div>
                             </div>
                             <div class="label">
                                 <h3>{!! $content['label'] !!}</h3>
                             </div>
                         </div>
                         @if (isset($content['link']))
-                        <a class="icon-link" href="{!! $content['link'] !!}" target="_blank" rel="noopener" title="{!! $content['label'] !!}">
+                        <a
+                            class="icon-link"
+                            href="{!! $content['link'] !!}"
+                            target="_blank"
+                            rel="noopener"
+                            title="{!! $content['label'] !!}"
+                        >
                             <span class="invisible">{!! $content['label'] !!}</span>
                         </a>
                         @endif
