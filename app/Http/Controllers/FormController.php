c723c99621c08889037ed48fb22f7aa429a012ec
Matthew Walcher <mwalcher@live.ca>
2019-03-02 10:17:40 -0500

    added fallback to validate email if recaptcha is not set

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -15,50 +15,55 @@ class FormController extends Controller
 
         $googleHelper = new GoogleHelpers();
 
-        if($googleHelper->verifyCaptcha($data['g-recaptcha-response'])->success === true){
+        if($data['g-recaptcha-response'] && $googleHelper->verifyCaptcha($data['g-recaptcha-response'])->success === true){
             unset($data['g-recaptcha-response']);
-
-            $rules = [
-                'name' => 'required',
-                'email' => 'required|email',
-                'message' => 'required',
-            ];
-
-            $messages = [
-                'required' => ':attribute is required',
-                'email' => ':attribute is not a valid email'
-            ];
-
-            $validator = Validator::make($data, $rules, $messages);
-
-            if ($validator->fails()) {
-                return redirect(URL::previous() . '#contact')
-                    ->withErrors($validator)
-                    ->withInput();
-            }
-
-            unset($data['_token']);
-            $data['email'] = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
-
-            $to = 'matt@mwalcher.com';
-            $subject = 'Website Contact Form';
-            $message = '';
-
-            foreach($data as $key => $content){
-                $message .= "<strong>" . ucfirst($key) . "</strong>: " . $content . "<br />";
-            }
-
-            $headers = array(
-                "MIME-Version: 1.0",
-                "Content-type: text/html; charset=iso-8859-1",
-                "From: Matthew Walcher <no-reply@mwalcher.com>",
-                "Reply-To: <{$data['email']}>",
-                "X-Mailer: PHP/".phpversion().""
-            );
-
-            mail($to, $subject, $message, implode("\r\n", $headers));
+            validateAndSendEmail($data);
+        } else if(!$data['g-recaptcha-response']) {
+            validateAndSendEmail($data);
         }
 
         return redirect()->route('thank-you');
+    }
+
+    private function validateAndSendEmail(Array $fields){
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ];
+
+        $messages = [
+            'required' => ':attribute is required',
+            'email' => ':attribute is not a valid email'
+        ];
+
+        $validator = Validator::make($fields, $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect(URL::previous() . '#contact')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        unset($fields['_token']);
+        $fields['email'] = filter_var($fields['email'], FILTER_SANITIZE_EMAIL);
+
+        $to = 'matt@mwalcher.com';
+        $subject = 'Website Contact Form';
+        $message = '';
+
+        foreach($fields as $key => $content){
+            $message .= "<strong>" . ucfirst($key) . "</strong>: " . $content . "<br />";
+        }
+
+        $headers = array(
+            "MIME-Version: 1.0",
+            "Content-type: text/html; charset=iso-8859-1",
+            "From: Matthew Walcher <no-reply@mwalcher.com>",
+            "Reply-To: <{$fields['email']}>",
+            "X-Mailer: PHP/".phpversion().""
+        );
+
+        mail($to, $subject, $message, implode("\r\n", $headers));
     }
 }
