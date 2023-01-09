9ada627151f4f97b9dca9d9d3494e5eb7a311d9a
Matthew Walcher <matt@mwalcher.com>
2023-01-09 16:31:09 +0000

    add base styles and typography

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>mwalcher</h1>
+  <h1>Matthew Walcher</h1>
 </template>
+
+<style lang="scss" module>
+  h1 {
+    text-align: center;
+  }
+</style>
