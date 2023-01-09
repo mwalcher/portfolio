31d091781f4058c53c2aee88fd003ccd7a27977c
Matthew Walcher <matt@mwalcher.com>
2023-01-09 12:31:12 +0000

    replace laravel with vue vite

--- resources/views/components/head/styles.blade.php
+++
@@ -1,6 +0,0 @@
-<link href="https://fonts.googleapis.com/css?family=Montserrat:900|Poppins:300,500" rel="stylesheet">
-<link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-<noscript>
-    <link rel="stylesheet" href="{{ mix('css/noscript.css') }}">
-</noscript>
