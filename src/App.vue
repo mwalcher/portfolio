ac789e888c8d351fc9012fa8d661514342e7d9c2
Matthew Walcher <matt@mwalcher.com>
2023-01-09 16:01:22 +0000

    remove base content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>mwalcher</h1>
 </template>
