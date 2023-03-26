6a51dd5c1d3b0a6b69e25730a9a59abf78533bcd
Matthew Walcher <matt@mwalcher.com>
2023-03-26 15:06:14 +0100

    move normalize to main styles

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import 'normalize.css';
 import '@/assets/styles/main.scss';
-
 import App from '@/App.vue';
 import router from '@/router';
 
