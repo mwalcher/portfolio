e7cf36e99a27a9dc263e0c30023e2430c9dc0095
Matthew Walcher <matt@mwalcher.com>
2023-03-26 15:03:37 +0100

    fix style ordering

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from '@/App.vue';
-import router from '@/router';
-
 import 'normalize.css';
 import '@/assets/styles/main.scss';
+
+import App from '@/App.vue';
+import router from '@/router';
 
 const app = createApp(App);
 
