b639a90d5214d4de95ac7aa355566c33eb07f77a
Matthew Walcher <matt@mwalcher.com>
2023-07-13 18:16:18 +0100

    add simple layout contact links

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import TabContent from '@/components/TabContent.vue';
 import HeaderLogo from '@/components/header/HeaderLogo.vue';
+import { githubContact, linkedinContact } from '@/constants/navigation';
 import { isAnchorLink } from '@/helpers';
 import type { TabContentProp } from '@/types/components';
+import type { Menu } from '@/types/navigation';
 
 withDefaults(
   defineProps<{
@@ -19,6 +21,8 @@ withDefaults(
     simpleLayout: false,
   },
 );
+
+const contactLinks: Menu = [githubContact, linkedinContact];
 </script>
 
 <template>
@@ -46,6 +50,20 @@ withDefaults(
       >
         {{ buttonText }}
       </RouterLink>
+
+      <div v-if="simpleLayout" :class="$style['contact-links']">
+        <a
+          v-for="contactItem in contactLinks"
+          :key="contactItem.label"
+          :href="contactItem.link"
+          target="_blank"
+          rel="noopener"
+          :aria-label="contactItem.fullLabel"
+        >
+          <span class="fa" :class="[contactItem.icon, $style.icon]" :aria-hidden="true" />
+          <span>{{ contactItem.label }}</span>
+        </a>
+      </div>
     </div>
 
     <TabContent v-if="tabContentList" :tabContentList="tabContentList" />
@@ -106,6 +124,41 @@ withDefaults(
 
     .cta {
       @include spacing(margin, top, 1.5rem);
+    }
+
+    .contact-links {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      @include spacing(padding, top, 1rem);
+      @include spacing(margin, left right, auto);
+
+      a {
+        color: $white-off;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        &:hover,
+        &:focus {
+          .icon {
+            color: $primary;
+          }
+        }
+
+        & ~ a {
+          @include spacing(margin, left, md);
+        }
+
+        .icon {
+          display: block;
+          color: $white-off;
+          font-size: 2.5rem;
+          @include spacing(padding, bottom, 0.5rem);
+          @include transition(color);
+        }
+      }
     }
   }
 }
