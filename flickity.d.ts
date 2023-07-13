8d9c6566121894844bd5522fc33e1bb6f4bf2e04
Matthew Walcher <matt@mwalcher.com>
2023-07-13 17:26:36 +0100

    fix type error

--- flickity.d.ts
+++ flickity.d.ts
@@ -1,6 +1,6 @@
 declare module 'flickity' {
   const Flickity: {
-    new (el: string | HTMLElement, options?: Record<string, unknown>): this;
+    new (el: string | HTMLElement, options?: Record<string, unknown>);
     destroy();
     resize();
   };
